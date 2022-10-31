d1bbf69abeff65f367fbdcddd9f235953ed64717
nathaliabrazk <nathaliabrazk@gmail.com>
2022-10-31 03:11:33 -0300

    .

--- Entregas/Contagem de 1 a 100.c
+++ Entregas/Contagem de 1 a 100.c
@@ -1,29 +1,33 @@
-//Elabore um programa que realize a contagem de 1 ate 100 e nos multiplos de 5 imprima apenas M5, caso o numero seja multiplo de 10 imprima apenas M10 na tela, caso seja multiplo de 15 imprima apenas M15 e caso nao seja multiplo de 5,10 ou 15 imprima o valor atual da variavel.
+//Elabore um programa que realize a contagem de 1 atÃ© 100 e nos mÃºltiplos de 5 imprima apenas M5, caso o nÃºmero seja mÃºltiplo de 10 imprima apenas M10 na tela, caso seja mÃºltiplo de 15 imprima apenas M15 e caso nÃ£o seja mÃºltiplo de 5,10 ou 15 imprima o valor atual da variÃ¡vel.
 #include<stdio.h>
 int main(){
-	int i;
-	for(i=1;i<=100;i++){
-	if(i%1==0){
-		printf("%d\n",i);
-	}else{
-		if(i%2==0){
-			printf("%d\n",i);
-		}else{
-			if(i%3==0){
-				printf("%d\n",i);
-				}else{
-					if(i%4==0){
-						printf("%d\n",i);
-					}else{
-						if(i%5==0){
-							printf("%M5\n");
-						}
-					}
-				}
-			}
-		}
-	}
-	}
-	
-	
-	
+int i=0;
+//primeira parte M5
+    for(i=1;i<=100;i++){
+    if(i%5==0){
+    printf("M5\n");
+ }else{
+    printf("%d\n",i);            
+}
+        //M10
+        if(i%10==0){
+        printf("M10\n");
+        }else{
+        printf("%d\n", i);
+        }
+            //M15
+            if(i%15==0){
+            printf("M15\n");
+            }
+            for(i=1;i<=100;i++){
+            if(i%15==0){
+            printf("M15\n");
+            }else{
+            printf("%d", i);
+                }
+
+
+  }
+
+  }
+}
