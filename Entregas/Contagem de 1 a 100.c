c700d7a4486e685b382b43c88e85b63a40092487
nathaliabrazk <nathaliabrazk@gmail.com>
2022-10-31 01:46:46 -0300

    .

---
+++ Entregas/Contagem de 1 a 100.c
@@ -0,0 +1,35 @@
+//Elabore um programa que realize a contagem de 1 até 100 e nos múltiplos de 5 imprima apenas M5, caso o número seja múltiplo de 10 imprima apenas M10 na tela, caso seja múltiplo de 15 imprima apenas M15 e caso não seja múltiplo de 5,10 ou 15 imprima o valor atual da variável.
+#include<stdio.h>
+int main(){
+//primeira parte M5
+    for(i=1;i<=100;i++){
+    if(i%5==0){
+    printf("M5\n");
+ }else{
+    printf("%d\n",i);            
+}
+        //M10
+        if(i%10==0){
+        printf("M10\n");
+        }else{
+        printf("%d\n", i);
+        }
+            //M15
+            if(i%15==0){
+            printf("M15\n");
+            }
+            for(i=1;i<=100;i++){
+            if(i%15==0){
+            printf("M15\n");
+            }else{
+            printf("%d", i);
+                }
+
+
+  }
+
+  }
+}
+
+    
+
