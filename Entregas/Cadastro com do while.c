4e5ff255313d0d23e8973224a947ad375010b6db
nathaliabrazk <nathaliabrazk@gmail.com>
2022-11-08 15:39:17 -0300

    .

--- Entregas/Cadastro com do while.c
+++ Entregas/Cadastro com do while.c
@@ -13,11 +13,11 @@ char genero;//%c
 
 //nome
 printf("Informe o nome: ");
-scanf("%s",&nome);
+fgets(nome,50,stdin);//usado para armazenar nome composto
 
 //idade
 do{
-printf("Informe a idade: ");
+printf("\nInforme a idade: ");
 scanf("%d",&idade);
 if(idade>=0){
     printf("Idade valida!(maior que 0)\n");
@@ -32,8 +32,42 @@ if(idade<200){
 } while (idade<0||idade>200);
 
 //altura
+do{
+    printf("\nInforme a altura: ");
+    scanf("%f",&altura);
+    if(altura>0.10){
+        printf("Altura valida!(maior que 10cm)\n");
+    }else{
+        printf("Altura invalida!(menor que 10cm\n");
+    }
+    if(altura<3.0){
+        printf("Altura valida!(menor que 3m)\n");
+    }else{
+        printf("Altura invalida!(maior que 3m)\n");
+    }
+    }while(altura<0.10||altura>3.0);
+    fflush(stdin);
+    //genero
+    do{
+        printf("\nInforme o genero(f,m,p): ");
+        scanf("%c",&genero);
+        fflush(stdin);
+        if(genero!='f'&& genero!='m'&& genero!='p'){
+            printf("Genero invalido!\n");
+        }
+    }while(genero!='f'&& genero!='m' && genero!='p');
+    printf("Genero valido!\n");
 
-
-
+    //impressao de dados 
+    printf("\n----------DADOS INFORMADOS----------\n\n");
+    printf("Nome:%s",nome);
+    printf("Idade:%d\n",idade);
+    printf("Altura:%.2f\n",altura);
+    printf("Genero:%c\n",genero);
 
 }//fim da main
+
+
+
+
+
